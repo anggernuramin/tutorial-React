f3df92d1cbcaa7c59eb26c822eb4319d78a34a16
Angger Nur <amggern514@gmail.com>
2023-07-07 15:41:42 +0700

    initial commit

---
+++ tutorial-axios/src/App.jsx
@@ -0,0 +1,12 @@
+import Title from "./components/Title";
+import RequestGET from "./examples/RequestGET";
+function App() {
+  return (
+    <main>
+      <Title />
+      <RequestGET />
+    </main>
+  );
+}
+
+export default App;
